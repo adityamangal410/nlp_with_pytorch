670006af098c2a662b0f2303e9ea6f9da894d052
Aditya Mangal <amangal@verizonmedia.com>
2020-06-16 16:57:50 -0700

    First commit using cookiecutter

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='NLP using PyTorch, companion repo to my blog',
+    author='Aditya Mangal',
+    license='MIT',
+)
